0d4e98a078e200982d4f5e1215b7d0965fb19de8
TiagoOliveiraSantos <tiago.os29@gmail.com>
2022-08-29 20:52:19 -0300

    Novas atulizaçãos incluidas

---
+++ Terra_Media_Tuor_Site/JS/script.js
@@ -0,0 +1,22 @@
+/*Alert da pagina de contado
+--> Confirmação do envio do e-mail
+*/
+const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+const alert = (message, type) => {
+  const wrapper = document.createElement('div')
+  wrapper.innerHTML = [
+    `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+    `   <div>${message}</div>`,
+    '   <button type="submit" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+    '</div>'
+  ].join('')
+
+  alertPlaceholder.append(wrapper)
+}
+
+const alertTrigger = document.getElementById('liveAlertBtn')
+if (alertTrigger) {
+  alertTrigger.addEventListener('click', () => {
+    alert('E-mail enviado com sucesso!', 'success')
+  })
+}
